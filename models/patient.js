d25fd21d94ea8acf14a244bd87069ee7c34ae201
Anthony Edelstein <asedelstein@gmail.com>
2018-05-22 19:00:10 -0700

    push for html handlebars

--- models/patient.js
+++ models/patient.js
@@ -1,26 +1,7 @@
-var NewPatient = sequelize.define('NewPatient', {
-
-    Last_Name: {
-      type: Sequelize.STRING
-    },
-
-    First_Name: {
-        type: Sequelize.STRING
-    },
-
-    Address: {
-      type: Sequelize.INTEGER
-
-    },
-    Street: {
-        type: Sequelize.STRING
-    
-    },
-
-    Date_Of_Birth: {
-      type: Sequelize.INTEGER
-    },
-  },
-  {
-    freezeNewPatient: true // Model NewPatient will be the same as the model name instead of being pluralized
+module.exports = function(sequelize, DataTypes) {
+  var Patients = sequelize.define("Patient", {
+    text: DataTypes.STRING,
+    complete: DataTypes.BOOLEAN
   });
+  return Patients;
+};
